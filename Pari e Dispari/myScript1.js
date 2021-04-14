32875c93d93ad5984bad05c00132aaf86ded2a13
Tommaso Cavaletti <cavalettitommaso@gmail.com>
2021-04-14 12:00:21 -0400

    correzione pariDispari

--- Pari e Dispari/myScript1.js
+++ Pari e Dispari/myScript1.js
@@ -1,24 +1,56 @@
+//variabili
 let pariDispari = prompt("Scegli pari o dispari").toLowerCase();
 let numeroUtente = parseInt(Math.floor(prompt("Inserisci un numero da 1 a 5 (in caso di decimale sarà arrotondato per difetto)")))
+let numeroRandom = Math.random()
 
-function chiVince (num) {
-num = Math.ceil(Math.random() * 5);
 
-if ((num + numeroUtente) % 2 == 0) {
-    return "pari"
+//funzioni
+function oneToFive(num) {
+   return num = Math.ceil(num * 5);
 }
 
-else if ((num + numeroUtente) % 2 > 0) {
-    return "dispari"
+numeroComputer = oneToFive(numeroRandom)
+
+function chiVince(numOne, numTwo) {
+    let somma = numOne + numTwo 
+
+    return somma % 2 === 0
 }
+
+let control = chiVince(numeroComputer, numeroRandom)
+
+if(control && pariDispari == "pari") {
+    console.log("Ci hai azzeccato! complimenti")
+}
+
+else if(control && pariDispari == "dispari") {
+    console.log("Hai perso, mi dispiace")
+}
+
+else if(!control && pariDispari == "dispari") {
+    console.log("Ci hai azzeccato! complimenti")
+}
+
+else if(!control && pariDispari == "pari") {
+    console.log("Hai perso, mi dispiace")
+}
+/*
+
+
+
+
+function chiVince() {
+
 }
 
 
 
-if (chiVince(numeroUtente) === "pari") {
+
+
+if (chiVince(numeroUtente, ) === "pari") {
     console.log("Hai vinto!")
 }
 
 else if (chiVince(numeroUtente) === "dispari") {
     console.log("Hai perso!")
-}
+}*/
